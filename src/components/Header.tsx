bfc7e3b105561251208503727212121b203285bc
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-01-05 20:17:10 +0700

    style: init project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import { EB_Garamond } from "next/font/google";
+
+const garamond = EB_Garamond({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h1
+        className={`${garamond.className} text-[32px] font-bold text-[#03045E]`}
+      >
+        K
+      </h1>
+    </div>
+  );
 }
 
 export default Header;
