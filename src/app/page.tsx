e05d94cfbeeceeaf52ec17ec12fcbd887ea28879
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-01-06 14:26:43 +0700

    style: add content for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { Github, Linkedin, Mail, PhoneCall } from "lucide-react";
+
 // Home Page ("/")
 export default function Home() {
   return (
-    <div className="h-screen text-white">
-      <h1>I am Dang Khoa</h1>
-      <h1>Front-end Developer</h1>
+    <div className="mt-10 flex justify-between text-white lg:h-screen">
+      <div className="">
+        <h1 className="text-[24px] lg:text-[48px]">Hello There,</h1>
+        <h1 className="text-[24px] lg:text-[48px]">
+          I am <span className="font-bold text-[#ffc86a]">Khoa</span>
+        </h1>
+        <h1 className="text-[24px] lg:text-[64px]">Front-end Developer</h1>
+      </div>
+      {/* contact icon information */}
+      <div className="mt-[90px] flex flex-col items-center gap-4">
+        <div className="bg-primary-color h-[200px] w-[2px]" />
+        <Linkedin />
+        <Github />
+        <PhoneCall />
+        <Mail />
+      </div>
     </div>
   );
 }
