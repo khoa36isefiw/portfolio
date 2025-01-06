adbbb2cd94e0725a803b5077127a6ba69e64427b
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-01-06 12:26:45 +0700

    style: fix warning using '

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="h-screen text-white">
-      <h1>I'm Dang Khoa</h1>
+      <h1>I am Dang Khoa</h1>
       <h1>Front-end Developer</h1>
     </div>
   );
