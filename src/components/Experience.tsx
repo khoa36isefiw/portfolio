50ba457172450a4f7cb96fe6c4101431d2b5ffea
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2025-03-01 07:12:55 +0700

    style: add experiences

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import Container from "./Container";
+import { experiences } from "@/data/experiences";
 
-function Experience() {
+function Experiences() {
   const [height, setHeight] = useState<number>(0);
   const ref = useRef<HTMLDivElement | null>(null);
 
@@ -19,54 +20,53 @@ function Experience() {
   }, []);
   // console.log("height: ", height);
   return (
-    <Container className="z-1 relative mt-[60px] min-h-[300px] bg-[#3d3e42]">
+    <Container className="z-1 relative mt-[60px] min-h-[300px] bg-[#3d3e42] py-4">
       <h1 className="absolute left-0 top-0 translate-x-[50%] translate-y-[-50%] text-[46px] font-bold text-white">
         Experience
       </h1>
-      <div className="h-10" />
-      <div className="flex items-center">
-        {/* Year and Triangle */}
-        <div className="relative flex h-[40px] items-center justify-center bg-[#5d5e61] px-4">
-          2024
-          <div className="absolute -right-[20px] top-0 h-full w-[20px] bg-[#5d5e61] [clip-path:polygon(0%_0%,100%_50%,0%_100%)]"></div>
-        </div>
+      {experiences.map((exp, index) => (
+        <div key={index}>
+          <div className="h-10" />
+          <div className="flex items-center">
+            {/* Year and Triangle */}
+            <div className="relative flex h-[40px] items-center justify-center bg-[#5d5e61] px-4">
+              {exp.time}
+              <div className="absolute -right-[20px] top-0 h-full w-[20px] bg-[#5d5e61] [clip-path:polygon(0%_0%,100%_50%,0%_100%)]"></div>
+            </div>
 
-        <div className="mx-8 flex flex-col items-center">
-          <div className="h-5 w-5 rounded-full bg-primary-color drop-shadow-xl"></div>
-        </div>
+            <div className="mx-8 flex flex-col items-center">
+              <div className="h-5 w-5 rounded-full bg-primary-color drop-shadow-xl"></div>
+            </div>
 
-        <h1 className="text-[32px] font-bold">Front-end Developer</h1>
-      </div>
-      <div className="flex items-center">
-        <div className="invisible relative flex h-[40px] items-center justify-center bg-[#5d5e61] px-4">
-          2024
-          <div className="absolute -right-[20px] top-0 h-full w-[20px] bg-[#5d5e61] [clip-path:polygon(0%_0%,100%_50%,0%_100%)]"></div>
-        </div>
+            <h1 className="text-[28px] font-bold">Front-end Developer</h1>
+          </div>
+          <div className="flex items-center">
+            <div className="invisible relative flex h-[40px] items-center justify-center bg-[#5d5e61] px-4">
+              2024
+              <div className="absolute -right-[20px] top-0 h-full w-[20px] bg-[#5d5e61] [clip-path:polygon(0%_0%,100%_50%,0%_100%)]"></div>
+            </div>
 
-        <div className="mx-6 flex flex-col items-center">
-          <div
-            className={`mx-4 w-1 bg-yellow-500`}
-            style={{ height: `${height}px` }}
-          ></div>
+            <div className="mx-6 flex flex-col items-center">
+              <div
+                className={`mx-4 w-1 bg-yellow-500`}
+                style={{ height: `${height}px` }}
+              />
+            </div>
+            <div ref={ref} className="m-width-[50%]">
+              <h1 className="text-[20px]">{exp.company}</h1>
+
+              <p>{exp.description}</p>
+              <ul>
+                {exp.responsibilities.map((resp, idx) => (
+                  <li key={idx}>• {resp}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-        <div ref={ref}>
-          <h1 className="text-[20px]">Level Up Studio</h1>
-          <p>
-            Build a website to showcase the game company and the games it
-            creates, as well as the companies it partners with.
-          </p>
-          <ul>
-            <li>• Worked part-time as a Front-end developer.</li>
-            <li>
-              • Developed the website application based on the company
-              requirement.
-            </li>
-            <li>• Worked with the design team of the company.</li>
-          </ul>
-        </div>
-      </div>
+      ))}
     </Container>
   );
 }
 
-export default Experience;
+export default Experiences;
---
+++ src/data/experiences.tsx
@@ -0,0 +1,35 @@
+export interface IExperience {
+  time: number;
+  position: string;
+  company: string;
+  description: string;
+  responsibilities: string[];
+}
+
+export const experiences: IExperience[] = [
+  {
+    time: 2024,
+    position: "Front-end Developer",
+    company: "Level Up Studio",
+    description:
+      "Build a website to showcase the game company and the games it creates, as well as the companies it partners with.",
+    responsibilities: [
+      "Worked part-time as a Front-end developer.",
+      "Developed the website application based on the company requirement.",
+      "Worked with the design team of the company.",
+    ],
+  },
+  {
+    time: 2025,
+    position: "Front-end Developer",
+    company: "Tâm Luxury",
+    description: "Develop interfaces for internal company systems.",
+    responsibilities: [
+      "Worked and developed user interfaces provided from the design team.",
+      "Developed interfaces for payroll accounting, attendance calculation, jewelry inspection.",
+      "Developed employee salary viewing interface, sales revenue statistics.",
+      "Developed jewelry order app and services customer for sales.",
+      "Worked with back-end team to connect API.",
+    ],
+  },
+];
